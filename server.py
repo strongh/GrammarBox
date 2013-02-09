2f090402fadcaf6a8c28cc8c2f1dced11790e055
homer <homer.strong@gmail.com>
2013-02-09 14:23:35 -0800

    specify some tags

--- server.py
+++ server.py
@@ -1,7 +1,32 @@
 import web
 import nltk
+import nltk.tag, nltk.data
 import json
 from nltk.stem.wordnet import WordNetLemmatizer
+
+default_tagger = nltk.data.load(nltk.tag._POS_TAGGER)
+model = {
+    # verbs
+    'eat': 'VB',
+    'eats': 'VB',
+    'bark': 'VB',
+    'chase': 'VB',
+    'wash': 'VB',
+    'swim': 'VB',
+    'study': 'VB',
+    'jump': 'VB',
+    'hang': 'VB',
+    # adjectives
+    'brown': 'JJ',
+    'red': 'JJ',
+    # nouns
+    'fish':'NN',
+    'dish':'NN'
+    }
+
+tagger = nltk.tag.UnigramTagger(model=model, 
+                                backoff=default_tagger,
+                                cutoff=1)
 
 urls = (
     '/(.*)', 'GrammarBox'
@@ -15,12 +40,12 @@ class GrammarBox:
     def GET(self, text):
         if text:
             grams = nltk.word_tokenize(text)
-            tokens = [Lemmatizer.lemmatize(token) 
+            tokens = [Lemmatizer.lemmatize(token)
                       for token in grams]
-            tags = nltk.pos_tag(tokens)
+            tags = tagger.tag(tokens)
             return json.dumps(
-                [list(tag) + [token]
-                 for tag, token in zip(tags, grams)])
+                [list(tag) + [gram]
+                 for tag, gram in zip(tags, grams)])
 
 
 if __name__ == "__main__":
